8444a4011e7384597bc261164a48251b903c4b22
n.b.q.x <nbqxnbq@gmail.com>
2014-11-06 13:04:21 +0900

    fix document name

--- src/geo/parse.clj
+++ src/geo/parse.clj
@@ -10,24 +10,29 @@
                  (parse))]
     (first (:features data))))
 
+(defn- get-name [parsed-data]
+  (get-in parsed-data [:properties :name]))
+
 (defn- get-coordinates [parsed-data]
   (get-in parsed-data [:geometry :coordinates]))
 
 ;; usage: (spit "out.kml" (make-kml "/path/to/gpx"))
 (defn make-kml [^String gpx-file-path]
-  (let [data (-> (parse! gpx-file-path) get-coordinates)]
+  (let [data (parse! gpx-file-path)
+        coordinates (get-coordinates data)
+        name (get-name data)]
     (html (xml-declaration "UTF-8")
           [:kml {:xmlns "http://www.opengis.net/kml/2.2"
                  (keyword "xmlns:atom") "http://www.w3.org/2005/Atom"}
            [:Document
-            [:name "test"]
+            [:name name]
             [(keyword "atom:author") [(keyword "atom:name") ""]]
             [:description ""]
             [:Placemark
              [:Style [:LineStyle [:color "99ff0000"] [:width 6]]]
              [:LineString [:coordinates
                            (clojure.string/join ","
-                                                (map #(clojure.string/join "," %) data))]]]]])))
+                                                (map #(clojure.string/join "," %) coordinates))]]]]])))
 
 
 
